30ea549bde92060beab53da66baf044d66d1511a
ejkim <ugoo2457@gmail.com>
2021-01-26 00:57:11 +0900

    캔버스로 이미지 출력

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <h1>Components</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Components',
+  props: {
+    // msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <canvas ref="imageCanvas" v-bind:width="width" v-bind:height="height" class="canvas"></canvas>
+
+    {{images.length}}
+    {{images[4]}}
+
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Home from '@/components/Home.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Home
+  },
+  props:{
+    images:{
+      type: Array
+    }
+  },
+  data(){
+    return{
+
+      height: 400,
+      width: 400,
+
+      img:'',
+      ctx:'',
+
+      // height: window.innerHeight,
+      // width: window.innerWidth,
+
+    }
+  },
+  mounted() {
+
+
+    this.ctx = this.$refs.imageCanvas.getContext('2d')
+
+    this.drawImage(4)
+
+  },
+  methods:{
+    drawImage(inx){
+      //캔버스에 이미지 드로잉
+      let img = new Image ()
+      img.src = this.images[inx]
+
+      img.onload = () => {
+        // this.ctx.drawImage(img, this.width, this.height);
+        this.ctx.drawImage(img, 0, 0, this.width, this.height);
+      }
+
+    }
   }
 }
 </script>
+
+
+<style scoped>
+  .canvas {
+    /*width: 400px;*/
+    /*height: 400px;*/
+    border: 1px solid #000;
+  }
+</style>
